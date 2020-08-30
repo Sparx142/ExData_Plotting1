6e72b2bf887e9670859750abee71a852f28a2f42
Sparx142 <69580410+Sparx142@users.noreply.github.com>
2020-08-30 23:27:41 +0530

    Final_files

---
+++ plot1.R
@@ -0,0 +1,9 @@
+dataFile <- "../Downloads/exdata_data_household_power_consumption/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subsetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+globalActivePower <- as.numeric(subsetData$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
